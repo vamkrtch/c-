3cc8c7772a27a4ded485634c9f87b5e27f09e999
vamkrtch <90754448+vamkrtch@users.noreply.github.com>
2022-05-14 15:54:00 +0400

    Create ex4.cpp

---
+++ Part 2/ex4.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Jack on 26.11.21.
+//
+#include <iostream>
+int main()
+{
+    float m;
+    float n;
+    float h;
+
+    std::cin >> m >> n >> h;
+    std::cout << 0.5*(m + n)*h;
+}
