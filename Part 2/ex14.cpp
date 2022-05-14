a47eb91d8f0973eda5e55513687f4b2461270456
vamkrtch <90754448+vamkrtch@users.noreply.github.com>
2022-05-14 16:55:42 +0400

    Create ex14.cpp

---
+++ Part 2/ex14.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+  int x{};
+
+
+
+
+  std::cin >> x;
+    if (x % 2 == 0 && x % 7 == 3)
+    {
+    std::cout << "YES";
+    }
+    else
+        std::cout << "NO";
+    
+}
