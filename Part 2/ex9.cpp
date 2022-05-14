f3b42ef7c8f5ed0971d5aa2bb0bd46b88af26ec8
vamkrtch <90754448+vamkrtch@users.noreply.github.com>
2022-05-14 16:46:22 +0400

    ex 9 source

---
+++ Part 2/ex9.cpp
@@ -0,0 +1,14 @@
+//
+//  ex9.cpp
+//  Areg_lC++
+//
+//  Created by Jack on 26.11.21.
+//
+
+#include <iostream>
+int main()
+{
+    int n;
+    std::cin >> n;
+    std::cout << char('a' + n - 1 ) << " " << char('A' + n - 1 );
+}
