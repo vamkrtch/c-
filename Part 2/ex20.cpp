0f04883ed50cd02d820823d3832ee8bc06ecca3a
vamkrtch <90754448+vamkrtch@users.noreply.github.com>
2022-05-14 17:11:49 +0400

    Create ex20.cpp

---
+++ Part 2/ex20.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+int main()
+{
+    int a;
+    int b;
+    int c;
+    int d;
+    std::cin >> a >> b >> c >> d;
+    if (a == (-d) && b != (-c))
+    {
+        std::cout <<  "YES";
+    }
+    else
+    {
+        std::cout << "NO";
+    }
+    
+    
+}
