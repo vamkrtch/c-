a8038de255c167d6676695352de52a1587be14ba
vamkrtch <vam.golden.group.j@gmail.com>
2022-05-14 15:44:36 +0400

    Initial commit

---
+++ part 1/ex2.cpp
@@ -0,0 +1,18 @@
+//Գրել ծրագիր, որը կհաշվի և էկրանին կտպի -10 և 26 թվերի տարբերությունը։
+//
+//  main.cpp
+//  Areg_lC++
+//
+//  Created by Jack on 26.11.21.
+//
+#include <iostream>
+
+int main() {
+    int a;
+    int b;
+    a = -10;
+    b =  26;
+    std::cout << a - b;
+}
+
+
