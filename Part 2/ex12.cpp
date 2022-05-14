32411cf1bd14b99c74445d56aee9f075d23005b0
vamkrtch <90754448+vamkrtch@users.noreply.github.com>
2022-05-14 16:53:02 +0400

    Create ex12.cpp

---
+++ Part 2/ex12.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Jack on 24.11.21.
+//
+#include "iostream"
+int main()
+{
+    unsigned int araji_tiv;
+    unsigned int erkrord_tiv;
+
+    std::cin >> araji_tiv >> erkrord_tiv;
+    if (araji_tiv > erkrord_tiv)
+    {
+        std::cout << araji_tiv;
+
+    }
+    else
+        std::cout << erkrord_tiv;
+}
