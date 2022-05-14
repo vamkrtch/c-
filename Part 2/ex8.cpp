4da091b19b843effdc8c5d150738647cc10fb403
vamkrtch <90754448+vamkrtch@users.noreply.github.com>
2022-05-14 16:44:36 +0400

    ex 8 source file

---
+++ Part 2/ex8.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Jack on 26.11.21.
+//
+#include <iostream>
+int main()
+{
+    int a;
+    int b;
+    int c;
+    double d;
+    double sum;
+    std::cin >> a >> b >> c >> d;
+    sum = a + b + c;
+    std::cout << (sum/d);
+
+}
