c09d4768855b352bd8c63249b83249313996745a
vamkrtch <90754448+vamkrtch@users.noreply.github.com>
2022-05-14 15:56:10 +0400

    Create ex5.cpp

---
+++ Part 2/ex5.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Jack on 26.11.21.
+//
+#include <iostream>
+int main()
+{
+    float a;
+    float b;
+    float c;
+    float d;
+    std::cin >> a >> b >> c >> d;
+    std::cout << (a+b)-(c+d);
+}
