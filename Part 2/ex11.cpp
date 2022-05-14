114d0ea76f6e465a5d3f3dce22551595dd58e9b4
vamkrtch <90754448+vamkrtch@users.noreply.github.com>
2022-05-14 16:51:06 +0400

    Create ex11.cpp

---
+++ Part 2/ex11.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+unsigned int namber_1;
+unsigned int namber_2;
+std::cin >> namber_1 >> namber_2;
+    if (namber_1 == namber_2)
+    {
+        std::cout << "YES" << std::endl;
+    }
+    else
+        std::cout << "NO" << std::endl;
+
+}
